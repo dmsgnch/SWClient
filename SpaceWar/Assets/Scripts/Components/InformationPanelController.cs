9f5dfb4a6902ff16e0ce724bc7fa4e3cc92295ff
Єлизавета Коль <yelyzaveta.kol@nure.ua>
2023-04-30 16:22:26 +0300

    MovingInfoPanels

--- SpaceWar/Assets/Scripts/Components/InformationPanelController.cs
+++ SpaceWar/Assets/Scripts/Components/InformationPanelController.cs
@@ -18,19 +18,32 @@ namespace Components
         [SerializeField] private Sprite warningIcon;
 
         [SerializeField] private GameObject infoPanelPrefab;
-
+        [SerializeField] private List<GameObject> infoPanels;
         public void CreateMessage(MessageType msgType, string message)
         {
-            var infoPanel = Instantiate(infoPanelPrefab, GetComponent<Transform>());
-
+            var infoPanel = Instantiate(infoPanelPrefab,GetComponent<RectTransform>());
+            infoPanels.Add(infoPanel);
             Image objImage = infoPanel.transform.Find("IP_img").GetComponent<Image>();
             objImage.sprite = (int)msgType == 0 ? infoIcon : ((int)msgType == 1 ? errorIcon : warningIcon);
             objImage.preserveAspect = false;
 
             Text text = infoPanel.transform.Find("IP_tb_text").GetComponent<Text>();
             text.text = message;
+            ControlPanels();
         }
-
+        public void ControlPanels()
+        {
+            if (infoPanels.Count > 5)
+            {
+                Destroy(infoPanels[0]);
+                infoPanels.RemoveAt(0);
+            }
+            foreach(GameObject i in infoPanels)
+            {
+               i.transform.localPosition = i.transform.localPosition - new Vector3(0,50,0);
+            }
+            Debug.Log(infoPanels.Count);
+        }
         public enum MessageType
         {
             INFO = 0,
