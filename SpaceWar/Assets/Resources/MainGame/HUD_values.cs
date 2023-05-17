fc36e61a552bcec0b14ef58c7d2147c67c91e86e
uniqueVlaB <129681403+uniqueVlaB@users.noreply.github.com>
2023-05-17 17:55:55 +0300

    added statusbar and turnPanel

---
+++ SpaceWar/Assets/Resources/MainGame/HUD_values.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Resourses.MainGame
+{
+    public static class HUD_values {
+        public static int totalNumResourses = 13945;
+        public static int totalNumSoldiers = 45646;
+        public static int totalNumResearchShips = 322556;
+        public static int totalNumColonizationShips = 346574;
+        public static int usedNumSoldiers = 0;
+        public static int usedNumColonizationShips = 0;
+        public static int usedNumResearchShips = 0;
+        public static int timeLeft = 45;
+        public static string currentTurnHeroName = "SuperCum";
+    }
+}
