2f43231719efa03b669c4ebf8b7ac775b6396257
DmitriyGG03 <dimchikdg89@gmail.com>
2023-05-29 20:32:35 +0300

    Add some example

---
+++ SpaceWar/Assets/Scripts/View/HUDView.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Scripts.ViewModels;
+using Assets.View.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.View
+{
+	public class HUDView : AbstractScreen<HUDViewModel>
+	{
+		[SerializeField] private GameObject playerList;
+		[SerializeField] private GameObject playerListItemPrefab;
+		[SerializeField] private Button startButton;
+		[SerializeField] private GameObject readyButton;
+
+		private HUDViewModel _hudViewModel;
+
+		private void Awake()
+		{
+			startButton.onClick.AddListener(OnStartButtonClick);
+
+		}
+
+		public void OnStartButtonClick()
+		{
+			_hudViewModel.CreateMessage();
+			//Reference on view model
+		}
+
+		private void OnEnable()
+		{
+			if (_hudViewModel is null) return;
+
+			//Functionality
+		}
+
+		protected override void OnBind(HUDViewModel hudViewModel)
+		{
+			_hudViewModel = hudViewModel;
+		}
+	}
+}
