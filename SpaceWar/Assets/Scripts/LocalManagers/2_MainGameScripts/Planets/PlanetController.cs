cd697ceb088bfee5f6ffac73f987c04a01a7e069
DmitriyGG03 <dimchikdg89@gmail.com>
2023-06-02 16:07:22 +0300

    Repair Buttons list generating

--- SpaceWar/Assets/Scripts/LocalManagers/2_MainGameScripts/Planets/PlanetController.cs
+++ SpaceWar/Assets/Scripts/LocalManagers/2_MainGameScripts/Planets/PlanetController.cs
@@ -52,53 +52,68 @@ public class PlanetController : MonoBehaviour
     void CreateActMenu()
     {
         actMenu.transform.position = Input.mousePosition;
-        switch ((PlanetStatus)planet.Status) {
-            case PlanetStatus.Colonized:
-                actionNames = new List<string> { };
-                return;
-               // break;
-            case PlanetStatus.Known:
-                actionNames = new List<string> {"Research"};
-                break;
-            case PlanetStatus.Researched:
-                actionNames = new List<string> { "Colonize" };
-                break;
-            case PlanetStatus.Colonizing:
-                actionNames = new List<string> { };
-                return;
-              //  break;
-            case PlanetStatus.Enemy:
-                actionNames = new List<string> { "Attack" };
-                break;
-            case PlanetStatus.Unknown:
-                actionNames = new List<string> { };
-                return;
-              //  break;
-            case PlanetStatus.HasStation:
-                actionNames = new List<string> { };
-                return;
-               // break;
-            default:
-                throw new DataException();
-        }
-        for (int i = 0; i < actionNames.Count; i++)
-        {
-            GameObject buttonObject = Instantiate(ButtonPrefab, actMenu.transform);
-            Button button = buttonObject.GetComponent<Button>();
 
-            TMP_Text buttonText = buttonObject.GetComponentInChildren<TMP_Text>();
-            buttonText.text = actionNames[i];
-           
-            buttonObject.transform.localPosition = new Vector3(0f, -i * 30f, 0f);
 
-            int index = i;
-            button.onClick.AddListener(() => OnActionButtonClick(index));
-        }
-        actMenuEnabled = true;
+		for (int i = 0; i < actionNames.Count; i++)
+		{
+			GameObject buttonObject = Instantiate(ButtonPrefab, actMenu.transform);
+			Button button = buttonObject.GetComponent<Button>();
+
+			TMP_Text buttonText = buttonObject.GetComponentInChildren<TMP_Text>();
+			buttonText.text = actionNames[i];
+
+			buttonObject.transform.localPosition = new Vector3(0f, -i * 30f, 0f);
+
+			int index = i;
+			button.onClick.AddListener(() => OnActionButtonClick(index));
+		}
+		actMenuEnabled = true;
         DisableScripts();
     }
 
-    private void OnActionButtonClick(int index)
+    private List<string> GetButtonsListByPlanetStatus()
+    {
+		switch ((PlanetStatus)planet.Status)
+		{
+			case PlanetStatus.Known:
+				return new List<string> { "Research" };
+			case PlanetStatus.Researching:
+				return new List<string> { };
+			case PlanetStatus.Researched:
+				return new List<string> { "Colonize" };
+			case PlanetStatus.HasStation:
+				return new List<string> { };
+			case PlanetStatus.Colonizing:
+				return new List<string> { };
+			case PlanetStatus.Colonized:
+                return GetButtonsListByPlanetFortification();
+
+			case PlanetStatus.Enemy:
+                //TODO: Add checking for availability
+				return new List<string> { "Attack" };
+			default:
+				throw new DataException();
+		}
+	}
+
+	private List<string> GetButtonsListByPlanetFortification() 
+    {
+		switch ((Fortification)planet.FortificationLevel)
+		{
+			case Fortification.None:
+				return new List<string> { "Build low level fortifications (R)" };
+			case Fortification.Weak:
+				return new List<string> { "Build medium level fortifications (R)" };
+			case Fortification.Reliable:
+				return new List<string> { "Build total fortifications (R)" };
+			case Fortification.Strong:
+				return new List<string> { };
+			default:
+				throw new DataException();
+		}
+	}
+
+	private void OnActionButtonClick(int index)
     {
         Debug.Log($"Action button \"{actionNames[index]}\" clicked on planet \"{gameObject.name}\"");
         DestroyActMenu();
